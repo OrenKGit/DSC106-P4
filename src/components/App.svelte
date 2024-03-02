f03b9cff2099fb8028f4686f008761c7c2817416
EthanSLin <etl003@ucsd.edu>
2024-03-01 17:44:00 -0800

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,80 +1,78 @@
-<!-- App.svelte -->
 <script>
   import { onMount } from 'svelte';
   import { csvParse } from 'd3-dsv';
-  import { scaleLinear, scaleBand } from 'd3-scale';
-  import { max } from 'd3-array';
+  import { scaleLinear, max } from 'd3-scale';
   import { select } from 'd3-selection';
-  import { axisBottom, axisLeft } from 'd3-axis';
 
   let data = [];
 
   onMount(async () => {
+    // Fetch the CSV file
     const response = await fetch('count-hero-network.csv');
-    const csvData = await response.text();
-    data = csvParse(csvData, d => ({
+    const csvText = await response.text();
+
+    // Parse CSV data
+    data = csvParse(csvText, d => ({
       hero: d.hero1,
       value: +d.value
     }));
-    
-    createChart();
+
+    // Render chart
+    renderChart();
   });
 
-  function createChart() {
-    const svg = select('svg');
+  function renderChart() {
+    const svgWidth = 500;
+    const svgHeight = 300;
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
-    const width = +svg.attr('width') - margin.left - margin.right;
-    const height = +svg.attr('height') - margin.top - margin.bottom;
-    
-    const x = scaleBand().rangeRound([0, width]).padding(0.1);
-    const y = scaleLinear().rangeRound([height, 0]);
-    
-    const g = svg.append('g')
+    const width = svgWidth - margin.left - margin.right;
+    const height = svgHeight - margin.top - margin.bottom;
+
+    const svg = select('.chart-container')
+      .append('svg')
+      .attr('width', svgWidth)
+      .attr('height', svgHeight)
+      .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
-    
-    x.domain(data.map(d => d.hero));
-    y.domain([0, max(data, d => d.value)]);
-    
-    g.append('g')
-      .attr('class', 'axis axis-x')
-      .attr('transform', `translate(0,${height})`)
-      .call(axisBottom(x))
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .attr('dx', '-.8em')
-      .attr('dy', '.15em')
-      .attr('transform', 'rotate(-45)');
-    
-    g.append('g')
-      .attr('class', 'axis axis-y')
-      .call(axisLeft(y).ticks(10))
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 6)
-      .attr('dy', '0.71em')
-      .attr('text-anchor', 'end')
-      .text('Value');
-    
-    g.selectAll('.bar')
+
+    const x = scaleLinear()
+      .domain([0, max(data, d => d.value)])
+      .range([0, width]);
+
+    const y = scaleLinear()
+      .domain([0, data.length])
+      .range([height, 0]);
+
+    svg.selectAll('.bar')
       .data(data)
       .enter().append('rect')
       .attr('class', 'bar')
-      .attr('x', d => x(d.hero))
-      .attr('y', d => y(d.value))
-      .attr('width', x.bandwidth())
-      .attr('height', d => height - y(d.value));
+      .attr('x', 0)
+      .attr('y', (d, i) => y(i))
+      .attr('width', d => x(d.value))
+      .attr('height', y.bandwidth());
+
+    svg.selectAll('.bar-label')
+      .data(data)
+      .enter().append('text')
+      .attr('class', 'bar-label')
+      .attr('x', d => x(d.value))
+      .attr('y', (d, i) => y(i) + y.bandwidth() / 2)
+      .attr('dx', 3) // padding-left
+      .attr('dy', '.35em') // vertical-align: middle
+      .text(d => d.value);
   }
 </script>
 
-<svg width="960" height="500"></svg>
-
-
-<main>
-  <h1>Svelte template</h1>
- 
-  <p>Write your HTML here</p>
-</main>
+<div class="chart-container"></div>
 
 <style>
-  /* Write your CSS here */
+  .bar {
+    fill: steelblue;
+  }
+
+  .bar-label {
+    fill: white;
+    font: 10px sans-serif;
+  }
 </style>
